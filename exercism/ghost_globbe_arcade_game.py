2a3e668d6e9076b295d3559aacb38b67c9c342c6
Phernando <nandovalverde@gmail.com>
2022-04-29 14:03:29 +0100

    Corrigido comentário dentro da função que precisa ficar após a linha def

--- exercism/ghost_globbe_arcade_game.py
+++ exercism/ghost_globbe_arcade_game.py
@@ -43,61 +43,64 @@ False
 
 
 def eat_ghost(power_pellet_active, touching_ghost):
+    """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet.
+
+           :param power_pellet_active: bool - does the player have an active power pellet?
+           :param touching_ghost: bool - is the player touching a ghost?
+           :return: bool - can the ghost be eaten?
+    """
     if power_pellet_active and touching_ghost:
         return True
 
-        """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet.
-    
-        :param power_pellet_active: bool - does the player have an active power pellet?
-        :param touching_ghost: bool - is the player touching a ghost?
-        :return: bool - can the ghost be eaten?
-        """
+
     else:
         return False
 
 
 def score(touching_power_pellet, touching_dot):
+    """Verify that Pac-Man has scored when a power pellet or dot has been eaten.
+
+            :param touching_power_pellet: bool - does the player have an active power pellet?
+            :param touching_dot: bool - is the player touching a dot?
+            :return: bool - has the player scored or not?
+    """
     if touching_power_pellet or touching_dot:
         return True
 
-        """Verify that Pac-Man has scored when a power pellet or dot has been eaten.
-    
-        :param touching_power_pellet: bool - does the player have an active power pellet?
-        :param touching_dot: bool - is the player touching a dot?
-        :return: bool - has the player scored or not?
-        """
+
     else:
         return False
 
 
 def lose(power_pellet_active, touching_ghost):
+    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet.
+
+            :param power_pellet_active: bool - does the player have an active power pellet?
+            :param touching_ghost: bool - is the player touching a ghost?
+            :return: bool - has the player lost the game?
+    """
     if not power_pellet_active and touching_ghost:
         return True
 
-        """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet.
-    
-        :param power_pellet_active: bool - does the player have an active power pellet?
-        :param touching_ghost: bool - is the player touching a ghost?
-        :return: bool - has the player lost the game?
-        """
+
     else:
         return False
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+    """Trigger the victory event when all dots have been eaten.
+
+                  :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
+                  :param power_pellet_active: bool - does the player have an active power pellet?
+                  :param touching_ghost: bool - is the player touching a ghost?
+                  :return: bool - has the player won the game?
+    """
     if not power_pellet_active and touching_ghost:
         lost = True
     else:
         lost = False
     if has_eaten_all_dots and not lost:
         return True
-        """Trigger the victory event when all dots have been eaten.
-
-              :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
-              :param power_pellet_active: bool - does the player have an active power pellet?
-              :param touching_ghost: bool - is the player touching a ghost?
-              :return: bool - has the player won the game?
-              """
 
     else:
         return False
